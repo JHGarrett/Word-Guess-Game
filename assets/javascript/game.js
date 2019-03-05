c6a3bdf24f4f671607873bbca6f46f7871c63305
John Garrett <infantrygarrett@icloud.com>
2019-03-04 23:14:22 -0600

    added var to js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var words = ["thedarkknight", "gladiator", "findingnemo", "walle", "borat", "killbill", "inglouriousbasterds"]  //array for shows to guess
+var wordGuess = "";   //to get random show
+var wordLetters = []  //to split show and make an array of letters
+var underline = 0;    //to get the show blanks
+var wordBlank = [];   //to make the show blanks an array
+var wrong = [];       //to get array of wrong letters 
+var wins = 0;         //to hold amount of wins
+var losses = 0;       //to hold amount of losses
+var guessesLeft = 7;  //number of guesses
