daa15f9e9316ac0a7f4ca4d79bc5730e098d7f3e
John Garrett <infantrygarrett@icloud.com>
2019-03-05 07:34:50 -0600

    added function to track W/L

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,3 +19,60 @@ function movie() {   //function to get the random word and split
 
     document.getElementById("currentword").innerHTML = "  " + wordBlank.join("  ");    
 }  //end function movie
+
+function reset() {   //function to reset game
+    guessesLeft = 7;
+    wrong = [];
+    wordBlank = [];
+    movie()         
+} //end function reset
+
+function checkLetters(letter) {   //to check letters against random show
+    var letterInWord = false;    
+    for (var i = 0; i < underline; i++) {
+        if (wordGuess[i] == letter) {
+            letterInWord = true;
+        } //end if
+    }  //end for
+
+
+    if (letterInWord) {        
+        for (var i = 0; i < underline; i++) {
+            if (wordGuess[i] == letter) {
+                wordBlank[i] = letter;
+            } //end if
+        } //end for
+    } //end if
+    
+    else {
+        wrong.push(letter);
+        guessesLeft--;
+    } //end else    
+}  //end function checkLetters(letter)
+
+
+function complete() {    //function to reset after win or loss
+    if (wordLetters.toString() == wordBlank.toString()) {
+        wins++;
+        reset()        
+        document.getElementById("winstracker").innerHTML = " " + wins;       
+    } //end if
+
+    else if (guessesLeft === 0) {
+        losses++;
+        reset()
+        document.getElementById("image").src = ".assets/images/jollyRoger.jpg";                      
+        document.getElementById("losstracker").innerHTML = " " + losses;
+    } //end else if 
+    document.getElementById("currentword").innerHTML = "  " + wordBlank.join(" ");
+    document.getElementById("guessesLeft").innerHTML = " " + guessesLeft;    
+}  //end function complete
+
+movie()  //to call game to start
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();    
+    checkLetters(guesses);   
+    complete();     
+    console.log(guesses);    
+    document.getElementById("playerguesses").innerHTML = "  " + wrong.join(" ");
+}  //end onkeyup
